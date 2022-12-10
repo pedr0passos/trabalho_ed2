0ef4eda4dc5e829cfbf3c3b97fb9a9786ab3e8a2
Catterina <catterinavs@gmail.com>
2022-12-10 19:43:45 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pesquisa {
+    char nome[40];
+    int idade;
+    char sexo;
+    int musicas[5];
+}Pesquisa;
+
+void main(){
+    int lista[30];
+
+    FILE *arq;
+    fopen(arq, "r");
+
+}
