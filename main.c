87910d431056f0b81bb646f9096269be2eb517bf
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2023-01-01 21:48:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <locale.h>
 #include <string.h>
 
+#define TRUE 1
+#define FALSE 0 
+
+/*----------------------------------------------------------------------------
+                            ESTRUTURAS DO PROGRAMA:
+----------------------------------------------------------------------------*/
+
 typedef struct p {
     char nome[100];
     char sexo;
@@ -15,27 +22,75 @@ typedef struct lista {
     struct lista *proximo;
 }no;
 
+/*----------------------------------------------------------------------------
+                            CABEÇALHO DAS FUNÇÕES:
+----------------------------------------------------------------------------*/
+
 void limpa_buffer();
+void limpa_terminal();
 void cria_lista(no **lista);
 int vazia(no **lista);
-pessoa *le_pessoa()
+pessoa *le_pessoa();
+void insere(no **lista, pessoa nova);
+void imprime(no **lista);
+void linha();
+
+/*----------------------------------------------------------------------------
+                                    MAIN:
+----------------------------------------------------------------------------*/
 
 int main () {
 
-    // teste de pesquisa
-
-    pessoa p;
+    pessoa *p;
     no *l;
+    int laco = TRUE;
+    // criando a lista
     cria_lista(&l);
-
-    le
-
+    
+    int opcao;
+    while(laco) {
+        printf("1 - Adicionar Pesquisa\n");
+        printf("2 - Imprimir Pesquisa\n");
+        printf("OPCAO: ");
+        scanf("%d", &opcao);
+        switch (opcao) {
+        case 1:;
+            limpa_terminal();
+            p = le_pessoa();
+            insere(&l, *p);
+            limpa_terminal();
+        break;
+        case 2:;
+            limpa_terminal();
+            imprime(&l);
+        break; 
+        default:
+            printf("Programa Encerrado!");
+            laco = FALSE;
+        break;
+        }
+    }
     return 0;
 }
+/*----------------------------------------------------------------------------
+                            FUNÇÕES DO AUXILIARES:
+----------------------------------------------------------------------------*/
 
 void limpa_buffer() {
     setbuf(stdin, NULL);
 }
+
+void limpa_terminal(){
+    system("cls");
+}
+
+void linha() {
+    printf("\n\n-----------------------------------------------------------------------\n");
+}
+
+/*----------------------------------------------------------------------------
+                            FUNÇÕES DO PROGRAMA:
+----------------------------------------------------------------------------*/
 
 void cria_lista(no **lista){
     *lista = NULL;
@@ -46,9 +101,9 @@ int vazia(no **lista) {
 
 }
 
-no *le_no() {        
+pessoa *le_pessoa() {        
 
-    no *novo = (no*)malloc(sizeof(no));
+    pessoa *novo = (pessoa*)malloc(sizeof(pessoa));
 
     printf("Nome: ");
     limpa_buffer();
@@ -72,14 +127,24 @@ no *le_no() {
             i++;
     }
 
+    return novo;
 
 }
 
+void insere( no **lista, pessoa nova ) {
+    no *novo_no = malloc(sizeof(no));
+    novo_no->p = nova;
+    novo_no->proximo = (*lista);
+    (*lista) = novo_no;
+}
+
 void imprime (no **lista) {
+    linha();
     if (!vazia(lista)) {
         for (no *p = (*lista); p!= NULL; p = p->proximo )
-            printf("Nome: %[^\n] | Idade: %d | Sexo: %c", p->p.nome, p->p.idade, p->p.sexo);
+            printf("Nome: %s | Idade: %d | Sexo: %c\n", p->p.nome, p->p.idade, p->p.sexo);
     } else {
         printf("Lista Vazia");
     }
+    linha();
 }
