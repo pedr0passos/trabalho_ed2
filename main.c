22706e190a10e5ce7f37eb29d2cecfb7dbac0d83
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2023-01-05 12:12:11 -0300

    Atualizoes na Leitura do Arquivo, e Lista de Musicas Funcionando

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 // lista encadeada com toda a pesquisa
 
-/* 4 listas encadeadas que possuem as pessoas que mencionaram em primeiro lugar (posicao 0 do vetor) uma das tres musicas mais populares da ca:
+/* 4 listas encadeadas que possuem as pessoas que mencionaram em primeiro lugar (posicao 0 do vetor) uma das tres musicas mais populares entre as 30:
     - Masculino que possuem idade <= 20
     - Masculino que possuem idade > 20 
     - Feminino que possuem idade <= 20
@@ -55,9 +55,11 @@ void adiciona_pesquisa (musicas *m, int musica_escolhida);
 pessoa *le_pessoa(musicas *m);
 void cria_lista_musicas(musicas *m);
 void insere(no **lista, pessoa nova);
-void le_arquivo( FILE *arq, no **lista1, no **lista2, no **lista3, no **lista4 );
-void grava( no **l1, no **l2, no **l3, no **l4 );
-void encerra(no **l1, no **l2, no **l3, no **l4 );
+void le_arquivo( FILE *arq, no **lista1, no **lista2, no **lista3, no **lista4, musicas *m );
+void grava( no **l1, no **l2, no **l3, no **l4, musicas *m );
+void encerra(no **l1, no **l2, no **l3, no **l4, musicas *m );
+void insere_do_arquivo(no **lista, pessoa nova);
+void imprime_musicas(musicas *m, int tamanho);
 
 /*----------------------------------------------------------------------------
                                     MAIN:
@@ -87,13 +89,14 @@ int main () {
     if ( arquivo != NULL )
         printf("Arquivo Aberto\n");
 
-    le_arquivo(arquivo, &lista1, &lista2, &lista3, &lista4);
+    le_arquivo(arquivo, &lista1, &lista2, &lista3, &lista4, m);
 
     int opcao;
     while(laco) {
         printf("1 - Adicionar a Pesquisa\n");
         printf("2 - Imprimir Pesquisa\n");
-        printf("3 - Sair\n");
+        printf("3 - Musicas mais Populares\n");
+        printf("4 - Sair\n");
         printf("OPCAO: ");
         scanf("%d", &opcao);
         switch (opcao) {
@@ -160,8 +163,13 @@ int main () {
                     printf("Opcao Invalida!\n");                
             }
         break; 
+        case 3:;
+            linha();
+            imprime_musicas(m, MaxMusicas);
+            linha();
+        break;
         default:
-            encerra(&lista1, &lista2, &lista3, &lista4);
+            encerra(&lista1, &lista2, &lista3, &lista4, m);
             laco = FALSE;
         break;
         }
@@ -211,6 +219,7 @@ pessoa *le_pessoa(musicas *m) {
     limpa_buffer();
     scanf("%c", &novo->sexo);
 
+
     printf("Musicas (1-30):\n");
     int i = 0;
     while (i < 5) {
@@ -233,7 +242,6 @@ pessoa *le_pessoa(musicas *m) {
                 i++;
             }
         }
-
     }
 
     return novo;
@@ -247,11 +255,29 @@ void insere(no **lista, pessoa nova) {
     (*lista) = novo_no;
 }
 
+void insere_do_arquivo(no **lista, pessoa nova) {
+
+    no *novo_no = malloc(sizeof(no));
+    novo_no->p = nova;
+
+    if (!vazia(lista)) {
+        no *auxiliar;
+        for ( auxiliar = (*lista); auxiliar->proximo != NULL; auxiliar = auxiliar->proximo );
+        novo_no->proximo = NULL;
+        auxiliar->proximo = novo_no;
+    } else {
+        novo_no->proximo = NULL;
+        (*lista) = novo_no;
+    }
+
+
+}
+
 void imprime (no **lista) {
 
     if (!vazia(lista)) {
         for (no *p = (*lista); p!= NULL; p = p->proximo )
-            printf("Nome: %s | Idade: %d | Sexo: %c\n", p->p.nome, p->p.idade, p->p.sexo);
+            printf("Nome: %s | Idade: %d | Sexo: %c | Musicas: %d %d %d %d %d\n", p->p.nome, p->p.idade, p->p.sexo, p->p.musicas[0], p->p.musicas[1], p->p.musicas[2], p->p.musicas[3], p->p.musicas[4]);
     } else {
         printf("Lista Vazia!\n");
     }
@@ -274,44 +300,75 @@ void adiciona_pesquisa(musicas *m, int musica_escolhida) {
     }
 }
 
-void le_arquivo( FILE *arq, no **l1, no **l2, no **l3, no **l4 ) {
+void le_arquivo( FILE *arq, no **l1, no **l2, no **l3, no **l4, musicas *m ) {
     
     pessoa *p = malloc(sizeof(pessoa));
 
-    while ( fscanf(arq, "%[^\t]\t%c\t%d\t%d %d %d %d %d\n", p->nome, &p->sexo, &p->idade, &p->musicas[0], &p->musicas[1], &p->musicas[2], &p->musicas[2], &p->musicas[3], &p->musicas[4]) != EOF ) {
+    while ( fscanf(arq, "%[^\t]\t%c\t%d\t%d %d %d %d %d\n", p->nome, &p->sexo, &p->idade, &p->musicas[0], &p->musicas[1], &p->musicas[2], &p->musicas[3], &p->musicas[4]) != EOF ) {
 
         no *l = malloc(sizeof(no));
-        int aux, i=0;
 
-        if ( (p->idade <= 20) && (strcmp(&p->sexo, "M") == 0) ) {
-            insere(l1, *p);
-        }
+        if ( (p->idade <= 20) && (strcmp(&p->sexo, "M") == 0) )
+            insere_do_arquivo(l1, *p);
+
         if ( (p->idade > 20) && (strcmp(&p->sexo, "M") == 0) ) 
-            insere(l2, *p);
+            insere_do_arquivo(l2, *p);
 
         if ( (p->idade <= 20) && (strcmp(&p->sexo, "F") == 0) )
-            insere(l3, *p);
+            insere_do_arquivo(l3, *p);
 
         if ( (p->idade > 20) && (strcmp(&p->sexo, "F") == 0) )
-            insere(l4, *p);
+            insere_do_arquivo(l4, *p);
 
+        int auxiliar1, auxiliar2;
+        int i = 0;
+        while ( fscanf(arq, "%d %d\t", &auxiliar1, &auxiliar2) == 2 ) {
+            printf("%d %d\n", auxiliar1, auxiliar2);
+            m[i].musica = auxiliar1;
+            m[i].quantidade = auxiliar2;
+            i++;        
+        }
     }
+
 
 }
 
-void grava( no **l1, no **l2, no **l3, no **l4 ) {
+void grava( no **l1, no **l2, no **l3, no **l4, musicas *m ) {
 
     FILE *arq = fopen("pesquisa.txt", "w");
 
+    // gravando a lista1
     for ( no *temporario = (*l1); temporario != NULL; temporario = temporario->proximo ) {
         fprintf(arq,"%s\t%c\t%d\t%d %d %d %d %d\n", temporario->p.nome, temporario->p.sexo, temporario->p.idade, temporario->p.musicas[0], temporario->p.musicas[1], temporario->p.musicas[2], temporario->p.musicas[3], temporario->p.musicas[4]);
+    }
+    // gravando a lista2
+    for ( no *temporario = (*l2); temporario != NULL; temporario = temporario->proximo ) {
+        fprintf(arq,"%s\t%c\t%d\t%d %d %d %d %d\n", temporario->p.nome, temporario->p.sexo, temporario->p.idade, temporario->p.musicas[0], temporario->p.musicas[1], temporario->p.musicas[2], temporario->p.musicas[3], temporario->p.musicas[4]);
+    }
+    // gravando a lista3
+    for ( no *temporario = (*l3); temporario != NULL; temporario = temporario->proximo ) {
+        fprintf(arq,"%s\t%c\t%d\t%d %d %d %d %d\n", temporario->p.nome, temporario->p.sexo, temporario->p.idade, temporario->p.musicas[0], temporario->p.musicas[1], temporario->p.musicas[2], temporario->p.musicas[3], temporario->p.musicas[4]);
+    }
+    // gravando a lista4
+    for ( no *temporario = (*l4); temporario != NULL; temporario = temporario->proximo ) {
+        fprintf(arq,"%s\t%c\t%d\t%d %d %d %d %d\n", temporario->p.nome, temporario->p.sexo, temporario->p.idade, temporario->p.musicas[0], temporario->p.musicas[1], temporario->p.musicas[2], temporario->p.musicas[3], temporario->p.musicas[4]);
+    }
+    // gravando a lista de musicas 
+    for ( int i = 0; i < MaxMusicas; i++ ) {
+        fprintf(arq, "%d %d\t", m[i].musica, m[i].quantidade);
     }
 
 }
 
-void encerra(no **l1, no **l2, no **l3, no **l4 ) {
+void encerra(no **l1, no **l2, no **l3, no **l4, musicas *m ) {
     linha();
     printf("Programa Encerrado!\n");
     linha();
-    grava(l1, l2, l3, l4);
+    grava(l1, l2, l3, l4, m);
+}
+
+void imprime_musicas(musicas *m, int tamanho) {
+    printf("MUSICAS MAIS POPULARES:\n");
+    for ( int i = 0; i < tamanho; i++ ) 
+        printf("Musica: %d | Quantidade: %d\n", m[i].musica, m[i].quantidade);
 }
